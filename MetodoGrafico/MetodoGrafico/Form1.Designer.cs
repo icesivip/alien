649054b10499ade368cbb8c0baf7e3e9e527289a
juan-carvajal <39658930+juan-carvajal@users.noreply.github.com>
2018-09-23 19:22:19 -0500

    Temprana implementacion de metodo grafico
    Se añadio una temprana implementacion del metodo grafico con dos variables. Implementado en Visual C#.

---
+++ MetodoGrafico/MetodoGrafico/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace MetodoGrafico
+{
+    partial class MetodoGrafico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FuncionObjetivo = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.lblRest = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDibujar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // FuncionObjetivo
+            // 
+            this.FuncionObjetivo.AutoSize = true;
+            this.FuncionObjetivo.Location = new System.Drawing.Point(518, 14);
+            this.FuncionObjetivo.Name = "FuncionObjetivo";
+            this.FuncionObjetivo.Size = new System.Drawing.Size(90, 13);
+            this.FuncionObjetivo.TabIndex = 1;
+            this.FuncionObjetivo.Text = "Funcion Objetivo:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(594, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(129, 43);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(4, 18);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ThousandsSeparator = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(68, 18);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // lblRest
+            // 
+            this.lblRest.AutoSize = true;
+            this.lblRest.Location = new System.Drawing.Point(518, 104);
+            this.lblRest.Name = "lblRest";
+            this.lblRest.Size = new System.Drawing.Size(74, 13);
+            this.lblRest.TabIndex = 4;
+            this.lblRest.Text = "Restricciones:";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoScroll = true;
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 3, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(521, 131);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(202, 348);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 348);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "X";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 348);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Y";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(103, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 348);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Tipo";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(153, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 348);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Valor";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // btnDibujar
+            // 
+            this.btnDibujar.Location = new System.Drawing.Point(521, 485);
+            this.btnDibujar.Name = "btnDibujar";
+            this.btnDibujar.Size = new System.Drawing.Size(97, 23);
+            this.btnDibujar.TabIndex = 6;
+            this.btnDibujar.Text = "Dibujar";
+            this.btnDibujar.UseVisualStyleBackColor = true;
+            this.btnDibujar.Click += new System.EventHandler(this.btnDibujar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(624, 485);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 23);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MAX",
+            "MIN"});
+            this.comboBox1.Location = new System.Drawing.Point(523, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(65, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // MetodoGrafico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 520);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnDibujar);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.lblRest);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.FuncionObjetivo);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MetodoGrafico";
+            this.Text = "Metodo Grafico";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label FuncionObjetivo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label lblRest;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnDibujar;
+        private System.Windows.Forms.Button btnAgregar;
+        private modelo.Modelo model;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MetodoGrafico/MetodoGrafico/Form1.cs
@@ -0,0 +1,325 @@
+﻿using MetodoGrafico.modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodoGrafico
+{
+    public partial class MetodoGrafico : Form
+    {
+        public MetodoGrafico()
+        {
+            InitializeComponent();
+            inicializarModelo();
+ 
+        }
+
+
+        private void inicializarModelo()
+        {
+            model = new modelo.Modelo();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Pasando de " + tableLayoutPanel2.RowCount + " filas a " + (tableLayoutPanel2.RowCount + 1));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute,20F));
+            tableLayoutPanel2.Controls.Add(numericFormateado(), 0, tableLayoutPanel2.RowCount);
+            tableLayoutPanel2.Controls.Add(numericFormateado(), 1, tableLayoutPanel2.RowCount);
+            tableLayoutPanel2.Controls.Add(comboBoxFormateado(), 2, tableLayoutPanel2.RowCount);
+            tableLayoutPanel2.Controls.Add(numericFormateado(), 3, tableLayoutPanel2.RowCount);
+            tableLayoutPanel2.RowCount++;
+        }
+
+        private ComboBox comboBoxFormateado()
+        {
+            ComboBox c = new ComboBox();
+
+            c.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            c.Dock = System.Windows.Forms.DockStyle.Fill;
+            c.FormattingEnabled = true;
+            c.Items.AddRange(new object[] {
+            "<=",
+            ">=",
+            "="});
+            c.SelectedIndex = 0;
+          
+
+
+
+            return c;
+        }
+
+
+        private NumericUpDown numericFormateado()
+        {
+            NumericUpDown num = new NumericUpDown();
+            num.DecimalPlaces = 2;
+            num.Dock = System.Windows.Forms.DockStyle.Fill;
+            num.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            num.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            -2147483648});
+            return num;
+        }
+
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            model.clearRest();
+            model.xObj = (double)numericUpDown1.Value;
+            model.yObj = (double)numericUpDown2.Value;
+            model.tipoObj = comboBox1.SelectedItem.ToString();
+            for (int i = 1; i < tableLayoutPanel2.RowCount; i++)
+            {
+
+                NumericUpDown X = (NumericUpDown)tableLayoutPanel2.GetControlFromPosition(0, i);
+                NumericUpDown Y = (NumericUpDown)tableLayoutPanel2.GetControlFromPosition(1, i);
+                ComboBox TIPO=(ComboBox)tableLayoutPanel2.GetControlFromPosition(2, i);
+                NumericUpDown C = (NumericUpDown)tableLayoutPanel2.GetControlFromPosition(3, i);
+                double x = Decimal.ToDouble(X.Value);
+                double y = Decimal.ToDouble(Y.Value);
+                string tipo = TIPO.SelectedItem.ToString();
+                double coef = Decimal.ToDouble(C.Value);
+
+
+                model.addRest(x, y, tipo, coef);
+            }
+
+            //for (int i = 0; i < model.restricciones.Count; i++)
+            //{
+            //    for (int j = i+1; j < model.restricciones.Count; j++)
+            //    {
+            //        Punto p = model.corteEntreRestricciones(model.restricciones[i], model.restricciones[j]);
+            //        Console.WriteLine("X:{0}   Y:{1}", p.x, p.y);
+            //    }
+            //}
+            
+            panel1.Refresh();
+            
+            model.valorOptimo();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+    
+
+            Console.WriteLine("RePaint panel...");
+            var g = e.Graphics;
+
+            var p = sender as Panel;
+            Brush b = new SolidBrush(Color.Black);
+            Pen pen = new Pen(b);
+            //g.DrawRectangle(pen, 10, 10, 60, 60);
+            g.DrawLine(pen, 10, 10, 10, p.Height - 10);
+            Point origenEjes = new Point(10, p.Height - 10);
+            Point limiteEjeX = new Point(10, 10);
+            Point limiteGrafica = new Point(p.Width - 10, 10);
+            int tamEjeX = Math.Abs(p.Width - 20);
+            g.DrawLine(pen,10, p.Height-10, p.Width-10, p.Height - 10);
+            Point limiteEjeY = new Point(p.Width - 10, p.Height - 10);
+            int tamEjeY = Math.Abs(p.Height - 20);
+            Font f = new Font("H", 7, FontStyle.Regular);
+            g.DrawString("Y",f, b, 7-1, -2);
+            g.DrawString("X",f, b, p.Width-10-1, p.Height-13-3);
+            float divison = tamEjeY / 10;
+            float divison2 = tamEjeX / 10;
+            Console.WriteLine(tamEjeY);
+          
+            //dibujo de ejes
+            for (int i=1; i<=10; i++)
+            {
+                g.DrawLine(pen, origenEjes.X, origenEjes.Y - (divison * i), origenEjes.X + 5, origenEjes.Y - (divison * i));
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                g.DrawLine(pen, origenEjes.X + (divison2 * i), origenEjes.Y, origenEjes.X + (divison2 * i), origenEjes.Y - 5);
+            }
+
+
+            //logica dibujo vertices
+            List<Punto> puntos = model.darVerticesTotales();
+
+            if (puntos.Count > 0)
+            {
+
+                double maxX = puntos.Max(i => i.x);
+                double maxY = puntos.Max(i => i.y);
+
+                if (maxX == 0)
+                {
+                    maxX = 10;
+                }
+
+                if (maxY == 0)
+                {
+                    maxY = 10;
+                }
+
+                for (int i = 1; i <= 10; i++)
+                {
+
+                    g.DrawString(String.Format("{0:0.##}", maxY * (i / 10.0)), f, b, origenEjes.X + 5, origenEjes.Y - (divison * i) - 6);
+                }
+
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    g.DrawString(String.Format("{0:0.##}", maxX * (i / 10.0)), f, b, origenEjes.X + (divison2 * i) - 5, origenEjes.Y - 10 - 7);
+
+                }
+
+
+                List<Restriccion> rest = model.restricciones;
+                pen.DashPattern=new float[]{ 3, 2, 3, 2 };
+                foreach (Restriccion r in rest)
+                {
+                    List<Punto> cortes = r.corteEjes();
+                    cortes = cortes.Where(i => i.x >= 0 && i.y >= 0).ToList();
+                    if (cortes.Count == 2)
+                    {
+                        Point drawX1 = transformar(cortes[0], maxX, maxY, tamEjeX, tamEjeY, origenEjes);
+                        Point drawX2 = transformar(cortes[1], maxX, maxY, tamEjeX, tamEjeY, origenEjes);
+                        Console.WriteLine("X1:{0}  Y1:{1}  X2:{2}  Y2:{3}", drawX1.X, drawX1.Y, drawX2.X, drawX2.Y);
+                        Point[] points;
+                        b = new SolidBrush(Color.FromArgb(255/rest.Count, 0, 255, 0));
+                        if (r.tipo.Equals(r.MAYOR_IGUAL))
+                        {
+                            points =new Point[] { drawX2, drawX1, limiteEjeX, limiteGrafica,limiteEjeY};
+                            g.FillPolygon(b, points);
+                            g.DrawLine(pen, drawX1, drawX2);
+                        }
+                        else if (r.tipo.Equals(r.MENOR_IGUAL))
+                        {
+                            points = new Point[] { drawX1, origenEjes, drawX2 };
+                            g.FillPolygon(b, points);
+                            g.DrawLine(pen, drawX1, drawX2);
+                        }
+                        else
+                        {
+                            g.DrawLine(pen, drawX1, drawX2);
+                        }
+                        b = new SolidBrush(Color.Black);
+                        //g.DrawLine(pen, drawX1, drawX2);
+                    }else if(cortes.Count==1)
+                    {
+                        Point drawX1 = transformar(cortes[0], maxX, maxY, tamEjeX, tamEjeY, origenEjes);
+                        double y = (r.cX * maxX) / (-r.cY);
+                        Punto aux = new Punto(maxX, y);
+                        Point drawX2 = transformar(aux, maxX, maxY, tamEjeX, tamEjeY, origenEjes);
+                        Console.WriteLine("X1:{0}  Y1:{1}  X2:{2}  Y2:{3}", drawX1.X, drawX1.Y, drawX2.X, drawX2.Y);
+                        g.DrawLine(pen, drawX1, drawX2);
+                    }
+                }
+
+                
+
+
+                for (int i = 1; i <= 10; i++)
+                {
+
+                    g.DrawString(String.Format("{0:0.##}", maxY * (i / 10.0)), f, b, origenEjes.X + 5, origenEjes.Y - (divison * i) - 6);
+                }
+
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    g.DrawString(String.Format("{0:0.##}", maxX * (i / 10.0)), f, b, origenEjes.X + (divison2 * i) - 5, origenEjes.Y - 10 - 7);
+
+                }
+
+            }
+
+
+            //dibujo de ejes
+            for (int i = 1; i <= 10; i++)
+            {
+                g.DrawLine(pen, origenEjes.X, origenEjes.Y - (divison * i), origenEjes.X + 5, origenEjes.Y - (divison * i));
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                g.DrawLine(pen, origenEjes.X + (divison2 * i), origenEjes.Y, origenEjes.X + (divison2 * i), origenEjes.Y - 5);
+            }
+
+            //
+
+            //    public Point transformar(Punto p, )
+            //{
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Point[] points = new Point[4];
+            //points[0] = new Point(50, 50);
+            //points[1] = new Point(110, 50);
+            //points[2] = new Point(110, 110);
+            //points[3] = new Point(50, 110);
+            //Brush b = new SolidBrush(Color.Blue);
+            //g.FillRectangle(b, 50, 50, 60, 60);
+            //g.FillPolygon(b, points);
+
+        }
+
+
+        private Point transformar(Punto p , double maxX, double maxY, int tamEjeX, int tamEjeY, Point origenEjes)
+        {
+           
+            double px = p.x * (tamEjeX/maxX);
+            
+            double py = p.y *(tamEjeY/maxY);
+            return new Point((int)(origenEjes.X+px), (int)(origenEjes.Y - py));
+        }
+
+
+    }
+}
