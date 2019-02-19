bc3193e05da4653619957a437412667f8b7ca761
joancolina819 <37549304+joancolina819@users.noreply.github.com>
2019-02-19 13:15:22 -0500

    MPS web page

---
+++ MPSPage/bootstrap-3.4.1-dist/sidenav.js
@@ -0,0 +1,13 @@
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100px";
+  document.getElementById("pushTest").style.marginUp = "250px";
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("pushTest").style.marginUp = "0px";
+  
+}
+</script>
